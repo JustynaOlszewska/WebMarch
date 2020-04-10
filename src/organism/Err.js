665f9cc139dad398d8940b4398f8c8c033d8b1cf
Justyna Olszewka <wioza@wp.pl>
2020-04-10 20:31:31 +0200

    Updates

--- src/organism/Err.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import '../style/sass/_err.scss'
-const Err = () => {
-    return ( 
-        <div className="err">Niewłaściwy url</div>
-     );
-}
- 
-export default Err;
