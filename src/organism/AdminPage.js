fe5c768d9cb86323b6e36f6ce69f26101bd73465
Justyna Olszewka <wioza@wp.pl>
2020-07-28 21:01:42 +0200

    Changing the admin page and the color of the application.

--- src/organism/AdminPage.js
+++ src/organism/AdminPage.js
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Route, Redirect} from 'react-router-dom';
 import './Login.js';
 const permission = true;
+ 
+class AdminPage extends Component {
+    state = { 
+        data: []
+     }
+    componentDidMount(){
+        fetch('https://randomuser.me/api/?results=40')
+      .then(response=> response.json())
+      .then(data=> this.setState({data: data.results}))
 
-const AdminPage = () => {
-    return ( 
-    <Route render={()=>(permission ? <h1>AdminPage</h1> : <Redirect to="/login"/> )}/>
-    )}
+    }
+    render() { 
+
+        const myData = this.state.data.map(result=> {
+            return (
+               
+                      <figure className='dataUsers'>
+                          <img className="dataUsers__image" src= {result.picture.large}  alt="whoReadAboutHealth"/>
+                             <figcaption className='dataUsers__name'>
+                             {result.name.first} {result.name.last}    
+                             </figcaption>
+                          
+                         </figure>  
+                      
+            )        
+        })
+
+        console.log(myData)
+        return ( 
+            <div className='listUsers'>
+    <Route render={()=>(permission ? myData : <Redirect to="/login"/> )}/>
+    </div>
+    )
+    }
+}
+ 
 export default AdminPage;
+
+   
--- src/organism/Login.js
+++ src/organism/Login.js
@@ -2,34 +2,34 @@ import React from 'react';
 import { useHistory, useLocation } from 'react-router-dom';
 import '../style/sass/_login.scss'
 const fakeAuth = {
-    isAuthenticated: false,
-    authenticate(cb) {
-      fakeAuth.isAuthenticated = true;
-      setTimeout(cb, 100); 
-    },
-    signout(cb) {
-      fakeAuth.isAuthenticated = false;
-      setTimeout(cb, 100);
-    }
-  };
+  isAuthenticated: false,
+  authenticate(cb) {
+    fakeAuth.isAuthenticated = true;
+    setTimeout(cb, 100);
+  },
+  signout(cb) {
+    fakeAuth.isAuthenticated = false;
+    setTimeout(cb, 100);
+  }
+};
 
 const Login = () => {
 
-    let history = useHistory();
-    let location = useLocation();
-    let { from } = location.state || { from: { pathname: "/admin" } };
-    let login = () => {
-      fakeAuth.authenticate(() => {
-        history.replace(from);
-      });
-    };
-  
-    return (
-      <div className="loginWrapper">
-        <p>You must log in to view the page at {from.pathname}</p>
-        <button className="buttonLogin" onClick={login}>Log in</button>
-      </div>
-    );
-  }
- 
+  let history = useHistory();
+  let location = useLocation();
+  let { from } = location.state || { from: { pathname: "/admin" } };
+  let login = () => {
+    fakeAuth.authenticate(() => {
+      history.replace(from);
+    });
+  };
+
+  return (
+    <div className="loginWrapper">
+      <p>If you want to know the registered users of this website, you need to login.</p>
+      <button className="buttonLogin" onClick={login}>Log in</button>
+    </div>
+  );
+}
+
 export default Login;
