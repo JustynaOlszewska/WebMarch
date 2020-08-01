b1ced68c1c9086894aefb2b091d0bd569a2f1904
Justyna Olszewka <wioza@wp.pl>
2020-08-01 20:13:31 +0200

    adding one route in Main.js

--- src/templates/Main.js
+++ src/templates/Main.js
@@ -7,31 +7,33 @@ import Me from '../organism/Me.js';
 import showInformationVegetables from '../organism/showInformationVegetables.js';
 import AdminPage from '../organism/AdminPage.js'
 import "../style/sass/_main.scss";
-import {Switch, Route} from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 
 const Main = () => {
-    return ( 
-<main className="main">
-<Switch>
-          <Route path="/" exact component= {Home}>
-          </Route>
-          <Route path="/garden" exact component={Garden}>  
-          </Route>
-            <Route path="/garden/:vegetable" component={showInformationVegetables}>
-          </Route>
-          
-          <Route path="/login" component={Login}>         
-          </Route>
-          <Route path="/admin" component={AdminPage}></Route>
-        <Route path="/me" component={Me}>         
-          </Route>
+  return (
+    <main className="main">
+      <Switch>
+        <Route path="/WebMarch" component={Home}>
+        </Route>
+        <Route path="/" exact component={Home}>
+        </Route>
+        <Route path="/garden" exact component={Garden}>
+        </Route>
+        <Route path="/garden/:vegetable" component={showInformationVegetables}>
+        </Route>
 
-          <Route component= {Err}>
-          </Route>
-       </Switch>
-</main>
+        <Route path="/login" component={Login}>
+        </Route>
+        <Route path="/admin" component={AdminPage}></Route>
+        <Route path="/me" component={Me}>
+        </Route>
 
-     );
+        <Route component={Err}>
+        </Route>
+      </Switch>
+    </main>
+
+  );
 }
- 
+
 export default Main;
