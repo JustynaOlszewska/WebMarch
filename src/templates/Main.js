a4de97b5e8616f049f6e725cc29b4f7312fecc4e
Justyna Olszewka <wioza@wp.pl>
2020-08-11 20:18:15 +0200

    Application: component lazy and Suspense

--- src/templates/Main.js
+++ src/templates/Main.js
@@ -1,36 +1,40 @@
-import React from 'react';
-import Home from '../organism/Home.js';
-import Garden from '../organism/Garden.js';
-import Login from '../organism/Login.js';
-import Err from '../organism/Err.js';
-import Me from '../organism/Me.js';
-import showInformationVegetables from '../organism/showInformationVegetables.js';
-import AdminPage from '../organism/AdminPage.js'
-import "../style/sass/_main.scss";
+import React, { Suspense, lazy } from 'react';
 import { Switch, Route } from "react-router-dom";
+import "../style/sass/_main.scss";
+
+const Home = lazy(() => import('../organism/Home.js'));
+const Garden = lazy(() => import('../organism/Garden.js'));
+const Login = lazy(() => import('../organism/Login.js'));
+const Err = lazy(() => import('../organism/Err.js'));
+const Me = lazy(() => import('../organism/Me.js'));
+const showInformationVegetables = lazy(() => import('../organism/showInformationVegetables.js'));
+const AdminPage = lazy(() => import('../organism/AdminPage.js'));
+
 
 const Main = () => {
   return (
     <main className="main">
-      <Switch>
-        <Route path="/WebMarch" component={Home}>
-        </Route>
-        <Route path="/" exact component={Home}>
-        </Route>
-        <Route path="/garden" exact component={Garden}>
-        </Route>
-        <Route path="/garden/:vegetable" component={showInformationVegetables}>
-        </Route>
+      <Suspense fallback={<div>Wczytywanie...</div>}>
+        <Switch>
+          <Route path="/WebMarch" component={Home}>
+          </Route>
+          <Route path="/" exact component={Home}>
+          </Route>
+          <Route path="/garden" exact component={Garden}>
+          </Route>
+          <Route path="/garden/:vegetable" component={showInformationVegetables}>
+          </Route>
 
-        <Route path="/login" component={Login}>
-        </Route>
-        <Route path="/admin" component={AdminPage}></Route>
-        <Route path="/me" component={Me}>
-        </Route>
+          <Route path="/login" component={Login}>
+          </Route>
+          <Route path="/admin" component={AdminPage}></Route>
+          <Route path="/me" component={Me}>
+          </Route>
 
-        <Route component={Err}>
-        </Route>
-      </Switch>
+          <Route component={Err}>
+          </Route>
+        </Switch>
+      </Suspense>
     </main>
 
   );
