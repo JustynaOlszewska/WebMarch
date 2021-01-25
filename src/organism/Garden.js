6ca415840c8e85cf788e8a420f4064e663c6e927
Justyna Olszewka <wioza@wp.pl>
2021-01-25 23:13:57 +0100

    refactor Garden.js

--- src/organism/Garden.js
+++ src/organism/Garden.js
@@ -4,21 +4,23 @@ import { vegetables } from "../utility";
 import { PATH_ROUTER } from "../constants";
 
 const Garden = () => {
-  const newVegetables = vegetables.map((vegetable, index) => {
-    return (
-      <li className="newVegetables" key={index}>
-        <Link className="listvVegetables" to={`${PATH_ROUTER.garden}${vegetable}`}>
-          {vegetable}
-        </Link>
-      </li>
-    );
-  });
   return (
     <div className="listStyle">
       <h1 className="spanHead">Please select a vegetable from the list.</h1>
       <ul className="vegetables">
-        {newVegetables.length ? (
-          newVegetables
+        {vegetables.length ? (
+          vegetables.map((vegetable, index) => {
+            return (
+              <li className="newVegetables" key={index}>
+                <Link
+                  className="listvVegetables"
+                  to={`${PATH_ROUTER.garden}${vegetable}`}
+                >
+                  {vegetable}
+                </Link>
+              </li>
+            );
+          })
         ) : (
           <h1>Sorry, I will add some information about healthy eating soon.</h1>
         )}
