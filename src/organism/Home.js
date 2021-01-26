5f9b412575664a66a56d8cd951377e3fb04ca674
Justyna Olszewka <wioza@wp.pl>
2021-01-26 08:29:11 +0100

    refactor Home.js

--- src/organism/Home.js
+++ src/organism/Home.js
@@ -3,20 +3,18 @@ import "../style/sass/_home.scss";
 import { articles } from "../utility";
 
 const Home = () => {
-  const articlesList = articles.map((article) => {
-    return (
-      <ul key={article.title} className="articleList">
-        <h2 className="articleHSmaller">{article.title} </h2>
-        <p className="articleText">{article.text}</p>
-      </ul>
-    );
-  });
   return (
     <div className="articleWrapper">
       <h1 className="articleH">Several articles on health.</h1>
-
-      {articlesList.length ? (
-        articlesList
+      {articles.length ? (
+        articles.map((article) => {
+          return (
+            <ul key={article.title} className="articleList">
+              <h2 className="articleHSmaller">{article.title} </h2>
+              <p className="articleText">{article.text}</p>
+            </ul>
+          );
+        })
       ) : (
         <h1>I am sory, but we have nothing to show you</h1>
       )}
